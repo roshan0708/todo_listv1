d1af5879629607f36a7d9a834f9ebeaf82906672
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-04-12 15:28:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const date = require(__dirname+"/date.js");
+
+const app = express();
+ let items=["Buy Food","Cook Food","Sleep"];
+ let workItems = [];
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
+  let day = date.getDate();
+  
+  res.render("list", {
+    listTitle: day,
+    newListItems : items
+  });
+});
+
+app.get("/work", function(req,res){
+  res.render("list",{
+    listTitle: "Work List",
+    newListItems : workItems
+  });
+});
+
+app.get("/about",function(req,res){
+  res.render("about");
+});
+
+app.post("/", function(req,res){
+
+  if(req.body.list === "Work"){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    let item = req.body.newItem;
+     items.push(item);
+     res.redirect("/");
+  }
+});
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
